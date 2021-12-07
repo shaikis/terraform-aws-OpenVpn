6a8d3cb40b20ffe5a684a5aa660705011ea9a84b
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2021-12-07 06:33:09 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,3 +1,31 @@
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+
+
 resource "aws_instance" "openvpn" {
   ami                         = "ami-0d2f82a622136a696" //us-west-2
   instance_type               = "t2.micro"
