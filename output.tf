22f3c0d3928627b4ad78c00f2032103703df78fb
Ismails <shaik.urs@gmail.com>
2021-12-07 10:12:21 +0530

    added Readme.md

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "ec2_instance_public_ip" {
-    value = ""
+    value = aws_instance.openvpn.public_ip
+}
+
+output "ec2_instance_id" {
+    value = aws_instance.openvpn.id
+}
+
+output "ec2_instance_private_ip" {
+    value = aws_instance.openvpn.private_ip
+}
+
+output "ec2_instance_ipv6_address" {
+    value = aws_instance.openvpn.ipv6_addresses
 }
