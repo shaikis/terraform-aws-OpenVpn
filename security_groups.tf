4af15e8de706e988de281bacdd38c4e5f4da964d
Ismails <shaik.urs@gmail.com>
2021-12-08 21:27:24 +0530

    added ssh

--- security_groups.tf
+++ security_groups.tf
@@ -15,6 +15,12 @@ resource "aws_security_group" "openvpn" {
     protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
